d876d6390ceefc53fdec0b0d86dac96c8922937e
DarioGuastella <jimmyguastella@gmail.com>
2024-03-20 16:23:50 +0100

    fix

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -15,13 +15,13 @@ class ProductFactory extends Factory
         $category_ids = Category::all()->pluck('id');
 
         return [
-            'category_id' => $this->faker->randomElement($category_ids),
-            'name' => $this->faker->words(2, true),
-            'description' => $this->faker->paragraphs(4, true),
-            'image' => $this->faker->imageUrl(640, 480, 'café', true),
-            'isbn' => $this->faker->randomNumber(7, true) . $this->faker->randomNumber(6, true),
-            'price' => $this->faker->randomFloat(2, 20, 100),
-            'featured' => $this->faker->boolean(5), // Imposta featured con probabilità del 5%
+            'category_id' => fake()->randomElement($category_ids),
+            'name' => fake()->words(2, true),
+            'description' => fake()->paragraphs(4, true),
+            'image' => fake()->imageUrl(640, 480, 'café', true),
+            'isbn' => fake()->randomNumber(7, true) . fake()->randomNumber(6, true),
+            'price' => fake()->randomFloat(2, 20, 100),
+            'featured' => fake()->boolean(50) && fake()->numberBetween(1, 5),
         ];
     }
 }
